7621f072d79439dcc8263ca6085896ee9faf001e
Dawid Wesołowski <pasqudek33@gmail.com>
2022-03-12 18:15:42 +0100

    Progress

--- 1.js
+++ 1.js
@@ -1,5 +1,5 @@
 class Person {
-  constructor(index, N) {
+  constructor(index) {
     this.index = index;
     this.weights = {}
   }
@@ -27,7 +27,7 @@ class Person {
   }
 }
 
-const N = 100
+const N = 6
 const people = []
 for (let i = 0; i < N; i++) {
   people.push(new Person(i))
@@ -36,12 +36,22 @@ for (let i = 0; i < N; i++) {
 people.forEach(person => person.generateWeights(people.filter(p => p.index !== person.index)))
 
 const countAbilityToWork = (subset) => {
+  const keys = []
   let sum = 0
   for (let i = 0; i < subset.length; i++) {
-    const el = subset[i];
-    for (let j = i + 1; j < subset.length; j++) {
-      sum += el.weights[j]
+    const element = subset[i];
+    for (key in element.weights) {
+      if (keys.includes(key)) {
+        continue;
+      } else {
+        keys.push(key)
+      }
+      sum += element.weights[key]
     }
+    // for (let j = i + 1; j < subset.length; j++) {
+    //   sum += el.weights[j]
+    //   console.log(el)
+    // }
   }
   return sum
 }
@@ -49,24 +59,57 @@ const countAbilityToWork = (subset) => {
 const half = Math.ceil(people.length / 2)
 const A = people.slice(0, half)
 const B = people.slice(-half)
-// console.log(people)
 
-console.log(countAbilityToWork(A))
-console.log(countAbilityToWork([...A, B[0]]))
+
+// console.log(A)
+// console.log(B)
+console.log('-------------- START -------------')
 
 let k = 0;
-while (k < B.length - 2) {
-  const new_a = countAbilityToWork([...A, B[k]]);
+while (k < N) {
+  // if (A.length === 1 || B.length === 1) {
+  //   break;
+  // }
+
   const a = countAbilityToWork(A);
+  const b = countAbilityToWork(B);
+  let new_a = a
+  let new_b = b
+
+  if (B[k]) {
+    new_a = countAbilityToWork([...A, B[k]]);
+  }
+
+  if (A[k]) {
+    new_b = countAbilityToWork([...B, A[k]]);
+  }
+  
+  const diff_a = new_a - a;
+  const diff_b = new_b - b;
+  console.log(k)
   console.log(`a: ${a}   new_a: ${new_a}`)
-  if (new_a > a) {
-    A.push(B[k])
-    B.splice(k, 1);
+  console.log(`b: ${b}   new_b: ${new_b}`)
+  console.log(`diff_a: ${diff_a}   diff_b: ${diff_b}`)
+
+  if (diff_a > diff_b) {
+    if (new_a > a) {
+      A.push(B[k])
+      B.splice(k, 1);
+    }
+  } else if (diff_a < diff_b) {
+    if (new_b > b) {
+      B.push(A[k])
+      A.splice(k, 1);
+    }
   } else {
     k++
   }
 }
-console.log(countAbilityToWork(A))
-console.log(countAbilityToWork(B))
+// console.log(A)
+// console.log(A.length)
+// conssole.log(countAbilityToWork(A))
+// console.log(B)
+// console.log(B.length)
+// console.log(countAbilityToWork(B))
 
 
---
+++ result.js
@@ -0,0 +1,25 @@
+[
+  Person { index: 0, weights: { '1': 3, '2': -2, '3': 1 } },
+  Person { index: 1, weights: { '0': 3, '2': -5, '3': -1 } }
+]
+[
+  Person { index: 2, weights: { '0': -2, '1': -5, '3': 5 } },
+  Person { index: 3, weights: { '0': 1, '1': -1, '2': 5 } }
+]
+-------------- START -------------
+
+
+
+
+5
+[
+  Person { index: 0, weights: { '1': 3, '2': -2, '3': 1 } },
+  Person { index: 1, weights: { '0': 3, '2': -5, '3': -1 } }
+]
+
+
+3
+[
+  Person { index: 2, weights: { '0': -2, '1': -5, '3': 5 } },
+  Person { index: 3, weights: { '0': 1, '1': -1, '2': 5 } }
+]
