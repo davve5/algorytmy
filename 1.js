e9cae9ac85129eb36f11c0a1413a7dc28506aaf3
Dawid Wesołowski <pasqudek33@gmail.com>
2022-03-12 17:11:03 +0100

    calc groups sum

--- 1.js
+++ 1.js
@@ -5,6 +5,8 @@ class Person {
   }
 
   generateWeights(people) {
+    const WEIGHTS = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
+
     people.forEach(p => {
       const weight = WEIGHTS[Math.floor(Math.random() * WEIGHTS.length)];
       if (p.index === this.index) {
@@ -25,9 +27,7 @@ class Person {
   }
 }
 
-const N = 5
-const WEIGHTS = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
-
+const N = 6
 const people = []
 for (let i = 0; i < N; i++) {
   people.push(new Person(i))
@@ -36,16 +36,26 @@ for (let i = 0; i < N; i++) {
 people.forEach(person => person.generateWeights(people.filter(p => p.index !== person.index)))
 
 const countAbilityToWork = (subset) => {
-  return subset.reduce((sum, person, index, people) => {
-    const people_without_current = people.filter(p => p.index !== person.index)
-    return sum + people_without_current.reduce((acc, p) => acc + p.weights[index], 0)
-  }, 0)
+  let sum = 0
+  for (let i = 0; i < subset.length; i++) {
+    const el = subset[i];
+    for (let j = i + 1; j < subset.length; j++) {
+      sum += el.weights[j]
+    }
+  }
+  return sum
+  // return subset.reduce((sum, person, index, people) => {
+  //   const people_without_current = people.filter(p => p.index !== person.index)
+  //   return sum + people_without_current.reduce((acc, p) => acc + p.weights[index], 0)
+  // }, 0)
 }
 
 const half = Math.ceil(people.length / 2)
-const A = people.slice(0, half - 1)
+const A = people.slice(0, half)
 const B = people.slice(-half)
-console.log(people)
-// console.log(countAbilityToWork(A))
+// console.log(people)
+console.log(A)
+console.log(countAbilityToWork(A))
 // console.log(B)
+
 
